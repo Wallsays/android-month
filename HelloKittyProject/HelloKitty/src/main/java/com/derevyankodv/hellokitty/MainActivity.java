6af686d051418b53d5fce72cf70d5c3e3cab3108
Wallsays <derevyankodenis92@gmail.com>
2013-07-12 17:01:43 +0700

    First app

---
+++ HelloKittyProject/HelloKitty/src/main/java/com/derevyankodv/hellokitty/MainActivity.java
@@ -0,0 +1,34 @@
+package com.derevyankodv.hellokitty;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView tvHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvHello = (TextView) findViewById(R.id.textView);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onClick(View v){
+        tvHello.setText("Hello Kitty!");
+
+    }
+    
+}
